bc05c514e48f165bd08f8dfd2ac766f1a543fc3e
Jacob Piland <piland.jacob@gmail.com>
2021-06-12 12:34:35 -0500

    refactor and add tests for all methods

--- spec/shift_generator_spec.rb
+++ spec/shift_generator_spec.rb
@@ -1,4 +1,5 @@
-
+require 'time'
+require 'date'
 require './lib/shift_generator'
 
 RSpec.describe ShiftGenerator do
@@ -33,24 +34,47 @@ RSpec.describe ShiftGenerator do
       allow(shift).to receive(:random_five_digit_number).and_return(random_five)
 
       expect(shift.random_five_digit_number).to eq([3,2,7,1,5])
+
       shift.determine_keys
+
       expect(shift.a_key).to eq(32)
       expect(shift.b_key).to eq(27)
       expect(shift.c_key).to eq(71)
       expect(shift.d_key).to eq(15)
     end
 
-    it "can determine turn today's date into offset" do
+    it "can turn date into offset" do
       shift = ShiftGenerator.new
-      time_stub = "2021-06-11 20:09:42.846327 -0500"
+      time_stub = ("2021-06-11 20:09:42.846327 -0500")
+      strf_stub = ("06/11/21")
       allow(Time).to receive(:now).and_return(time_stub)
+      t = Time.now
+      allow(t).to receive(:strftime).and_return(strf_stub)
 
-      shift.offset
+      shift.determine_offset
+
       expect(shift.a_offset).to eq(6)
-
+      expect(shift.b_offset).to eq(6)
+      expect(shift.c_offset).to eq(4)
+      expect(shift.d_offset).to eq(1)
     end
 
+    it "can determine the total shift" do
+      shift = ShiftGenerator.new
+      time_stub = ("2021-06-11 20:09:42.846327 -0500")
+      strf_stub = ("06/11/21")
+      random_five = [3,2,7,1,5]
+      allow(shift).to receive(:random_five_digit_number).and_return(random_five)
+      allow(Time).to receive(:now).and_return(time_stub)
+      t = Time.now
+      allow(t).to receive(:strftime).and_return(strf_stub)
 
+      shift.create_total_shift_hash
 
+      expect(shift.total_shift[:a_shift]).to eq(38)
+      expect(shift.total_shift[:b_shift]).to eq(33)
+      expect(shift.total_shift[:c_shift]).to eq(75)
+      expect(shift.total_shift[:d_shift]).to eq(16)
+    end
   end
 end
